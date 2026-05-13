b647db61bc95d0b05ac1b98c7c133668880657f5
Danny0327 <juliethcamposbustos@gmail.com>
2026-05-12 22:57:13 -0500

    feat: HU-DEV-DYC-03-ss-crud-clientes implementar CRUD de clientes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import auth_router, empleados_router
+from app.routers import auth_router, empleados_router, clientes_router
 from app.config import settings
 
 app = FastAPI(
@@ -22,6 +22,7 @@ app.add_middleware(
 # Incluir routers
 app.include_router(auth_router, prefix="/api/v1")
 app.include_router(empleados_router, prefix="/api/v1")
+app.include_router(clientes_router, prefix="/api/v1")
 
 @app.get("/")
 def root():
